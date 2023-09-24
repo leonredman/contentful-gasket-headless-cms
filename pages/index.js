7761343d0d537ac6be94530f3ad2ebd7ffd8e297
Leon Redman <leon0150@godaddy.com>
2023-09-23 23:44:03 -0700

    added multi col to page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,52 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Head from '../components/head';
-import GasketEmblem from '@gasket/assets/react/gasket-emblem';
+// import GasketEmblem from '@gasket/assets/react/gasket-emblem';
 
-const pageStyle = { textAlign: 'center' };
-const logoStyle = { width: '250px', height: '250px' };
+import { createClient } from 'contentful';
+import ProductCard from '../components/ProductCard';
 
-export const IndexPage = () => (
+// const logoStyle = { width: '250px', height: '250px' };
+const pageStyle = { color: 'black' };
+const headlineStyle = { textAlign: 'center' };
+const containerStyle= { margin: '40px' };
+
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const res = await client.getEntries({ content_type: 'recipe' })
+
+  return {
+    props: {
+      recipes: res.items,
+      fullWidthMarquee: res.items
+    }
+  }
+}
+
+
+export const IndexPage = ( {recipes,marquee}) => (
+  console.log(recipes),
+
   <div style={ pageStyle }>
-    <Head title='Home'/>
-    <GasketEmblem style={ logoStyle }/>
-    <h1>"Hello World" Welcome to Gasket!</h1>
- 
-    <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
-    <p>Lets make a quick headless CMS using Contentful and Storybook.</p>
-    <p><a href='https://gasket.dev'>Learn Gasket</a></p>
+     <Head title='Home'/>
+      <h1 style={ headlineStyle }>Contentful Sandbox</h1> 
+        <h2 style={ headlineStyle }>Multi-column</h2>
+          <div style={ containerStyle } className='container'>
+             {recipes.map(recipe => (
+                <ProductCard key={recipe.sys.id} recipe={recipe}/>
+             ))}
+
+         
+        </div>
   </div>
+ 
+
 );
 
 export default IndexPage;
