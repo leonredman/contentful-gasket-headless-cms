45b4ebe389c693d08773ec9759a5905326c23517
leonredman <leonproart@yahoo.com>
2023-12-22 15:03:11 -0700

    init commit

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Gasket-Test-App
+# Gasket Contentful-Test-App
 
-Test Gasket App
+https://gasket.dev/#/docs/quick-start?id=start
+
+Test Gasket Contentful Headless CMS App - Sandbox
 
 ## Setup
 
@@ -10,4 +12,22 @@ cd Gasket-Test-App
 npm install
 
 npx gasket local
+
+npm install contentful
+
+npm install @contentful/rich-text-react-renderer
+
+Update gasket.config.js
+
+create env file and add space Id and Access key
+
+Links to Docs:
+https://gasket.dev/#/
+https://gasket.dev/#/docs/quick-start?id=quick-start
+
+To Run Gasket:
+gasket local
+
+To View app localhost:8080
+
 ```
---
+++ components/Marquee.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+export default function Marquee( {fullWidthMarquee} ) {
+    const {fullWidthImage, fullWidthHeading, fullWidtEyebrow, fullWidthDescription } = recipe.fields
+  return (
+    
+    <div className='marqueeContainer'>
+
+
+    <div className="heroBackground">
+       <div className="marqueeImage">
+          <Image
+            src={'https:' + fullWidthImage.fields.file.url } 
+            width={fullWidthImage.fields.file.details.image.width}
+            height={fullWidthImage.fields.file.details.image.height}
+            />
+        </div>
+        <div className='info'>
+          <div>New</div>
+            <div>Eyebrow</div>
+                <h1 className='marqueeTitle'> { title} </h1>
+              
+                <div className='marqueeDescription'> { documentToReactComponents(fullWidthDescription)} </div>
+                <button className='cta-style'>Get Started</button>
+            </div>
+      
+            <style jsx>{`
+         .marqueeImage { 
+              width: 100%;
+        } 
+      `}</style>
+
+    </div>
+    </div>
+  )
+}
