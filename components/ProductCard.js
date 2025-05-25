4d41f3939110ca7f297e1f3868a9e4fd594cb404
leonredman <leonproart@yahoo.com>
2025-05-25 11:40:09 -0700

    Make Cards Moblie responsive

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,57 +1,64 @@
-import React from 'react'
-import Image from 'next/image'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
+import React from "react";
 
 // import Link from 'next/link'
 
-
-
-
-export default function ProductCard( {recipe}) {
-    const { title, slug, cookingTime, thumbnail, method } = recipe.fields
+export default function ProductCard({ recipe }) {
+  const { title, slug, cookingTime, thumbnail, method } = recipe.fields;
   return (
-
     <div className="card">
-       <div className="featured">
-          <Image
-            src={'https:' + thumbnail.fields.file.url } 
-            width={thumbnail.fields.file.details.image.width}
-            height={thumbnail.fields.file.details.image.height}
-            />
-        </div>
-        <div className='info'>
-          <div>New</div>
-            <div>Eyebrow</div>
-                <h1 className='cardTitle'> { title} </h1>
-              
-                <div className='description'> { documentToReactComponents(method)} </div>
-                <button className='cta-style'>Get Started</button>
-            </div>
-      
-            <style jsx>{`
+      <div className="featured">
+        <Image
+          src={"https:" + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
+      <div className="info">
+        <h1 className="cardTitle"> {title} </h1>
+        <div className="description"> {documentToReactComponents(method)} </div>
+        <button className="cta-style">Get Started</button>
+      </div>
+
+      <style jsx>{`
         .card {
           display: inline-block;
-          width:31%;
+          width: 31%;
           margin: 10px;
+          vertical-align: top;
         }
+
         .info {
-          flex-direction: column; 
+          display: flex;
+          flex-direction: column;
         }
+
         .cardTitle {
-          font-family:arial;
+          font-family: arial;
         }
-        
+
         .description {
-          width:100%;
+          width: 100%;
         }
-        .cta-style{
+
+        .cta-style {
+          width: 160px;
           padding: 10px 30px;
-          background-color: white;
-          border-outline: 5px;
+          background-color: black;
+          color: white;
+          border: none;
           border-radius: 5px;
         }
-      `}</style>
 
+        @media (max-width: 768px) {
+          .card {
+            display: block;
+            width: 100%;
+            margin: 20px 0;
+          }
+        }
+      `}</style>
     </div>
-  )
+  );
 }
