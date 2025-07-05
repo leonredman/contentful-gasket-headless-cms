5859dbd02c007999cfdd431a77d887875bdd5170
leonredman <leonproart@yahoo.com>
2025-07-05 09:16:52 -0700

    added view port

--- cypress/e2e/starterTest.cy.js
+++ cypress/e2e/starterTest.cy.js
@@ -1,6 +1,8 @@
 describe("Marquee Component", () => {
   it("should load homepage and display marquee", () => {
     cy.visit("http://localhost:8080/"); // can switch to use Live Netlify https://dazzling-kelpie-426d25.netlify.app/
+    // step2: set viewport to 1920x1080 desktop
+    cy.viewport(1920, 1080);
 
     // Assert headline and description to render
     cy.get('[data-cy="headline"]').should("be.visible");
