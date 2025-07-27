90445262d9187501c52f682413e1c01769102aec
leonredman <leonproart@yahoo.com>
2025-07-26 22:57:11 -0700

    update babel config for Nex JS build - debugging error

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+// babel config may be causing override of Next.js default babel behavor
+// module.exports = {
+//   presets: ["@babel/preset-env", "@babel/preset-react"],
+// };
+
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: ["next/babel"], // restores Next.js defaults
+  env: {
+    test: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    },
+  },
 };
